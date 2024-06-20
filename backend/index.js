e50955b94a5489498603073a69f5753d3d68942f
x-saim <saimxamir@gmail.com>
2024-06-20 13:06:58 -0400

    add GET requests to get all books and book by id

--- backend/index.js
+++ backend/index.js
@@ -55,3 +55,33 @@ app.post('/books', async (request, response) => {
     response.status(500).send({ message: error.message });
   }
 });
+
+// GET all books from database
+
+app.get('/books', async (request, response) => {
+  try {
+    const books = await Book.find({});
+
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// GET Book by id
+app.get('/books/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const book = await Book.findById(id);
+
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
