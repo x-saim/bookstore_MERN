8163fe015227589d6024ee1355bfe5e55d078117
x-saim <saimxamir@gmail.com>
2024-06-20 12:33:40 -0400

    omit config file

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,9 @@ const app = express();
 app.listen(PORT, () => {
   console.log(`App is listening to port: ${PORT}`);
 });
+
+// Routes
+
+app.get('/', (request, response) => {
+  return response.status(234).send('Welcome to MERN Stack Tutorial');
+});
