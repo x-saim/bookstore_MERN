8b2a484782424ec6bd2774b2e909c554f469d75a
x-saim <saimxamir@gmail.com>
2024-06-20 12:39:00 -0400

    connect to mongodb via mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
-});
+// Connect DB
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database.');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // Routes
 
